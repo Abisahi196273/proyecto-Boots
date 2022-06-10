de079e82201aa3b6e1f25ad835de51ef33169b18
Abisahi196273 <107278248+Abisahi196273@users.noreply.github.com>
2022-06-10 15:17:45 -0500

    Update SOIA V4.py

--- SOIA V4.py
+++ SOIA V4.py
@@ -6,3 +6,12 @@ from setuptools import setup
 from os import path
 import re
 import datetime
+
+import os.path
+path= 'C:\Domain_User\config.ini'
+dirname = os.path.dirname(path)
+print(dirname)
+print(path)
+
+
+
